51bf0b86093d549fd2285176fd968d6207d703bc
Yeifreng <yeifrenjose@gmail.com>
2023-12-27 08:10:50 -0500

    modulo de dbz creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-page.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MainPageComponent
+  ]
+})
+export class DbzModule { }
